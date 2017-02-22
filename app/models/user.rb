db985b0f225e498dd2efbb8d0065736aa9395408
Santtiago <sant_882@hotmail.com>
2017-02-21 22:14:51 -0600

    User login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	has_secure_password
 	has_many :articles
 	before_save { self.email = email.downcase }
 	validates :username, presence: true,
@@ -10,9 +11,7 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, length: { maximum: 105 },
 				uniqueness: { case_sensitive: false },
 				format: { with: VALID_EMAIL_REGEX }
-
 	
-
 
 
 end
