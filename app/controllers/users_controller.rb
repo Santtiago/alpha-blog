dfec8cc38f93a118cc2e3fe13b30be5799e8bd34
Santtiago <sant_882@hotmail.com>
2017-02-27 22:29:02 -0600

    Pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
 	def index
-		@users = User.all
+		@users = User.paginate(page: params[:page], per_page: 2)
 	end
 
 	def new
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
 
 	def show
 
-		@user = User.find(params[:id])
+		@user          = User.find(params[:id])
+		@user_articles = @user.articles.paginate(page: params[:page], per_page: 1) 
 
 	end
 
